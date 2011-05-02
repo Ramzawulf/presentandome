e1feb3984f9dbd7add185b99a71d7285df1e3f17
Rene Zurita <rene.zurita.sanchez@gmail.com>
2011-05-02 14:26:54 -0500

    Funcionalidad de login avanzada, utilizando JQUI

--- application/views/main_v.php
+++ application/views/main_v.php
@@ -4,55 +4,145 @@
     <link rel="stylesheet" href="css/960/reset.css" type="text/css" />
     <link rel="stylesheet" href="css/960/960.css" type="text/css" />
     <link rel="stylesheet" href="css/960/text.css" type="text/css" />
+    <link rel="stylesheet" href="css/ui-lightness/jquery-ui-1.8.12.custom.css" type="text/css" />
     <script type="text/javascript" src="js/jquery-1.5.2.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.8.12.custom.min.js"></script>
+    <script type="text/javascript" src="js/ua_behavior.js"></script>
     <meta charset="utf-8">
     <title><?=$title;?></title>
     <style>
+        a{
+            text-decoration: none;
+            color: inherit;
+        }
         #wrapper{
             background-color: #dddddd;
         }
         #container{
             background-color: #dddddd;
         }
+        #user_activity{
+            height: 50px;
+            background-color: #cccccc;
+        }
+        #heading{
+            background-color: #bbbbbb;
+        }
+        #followUp_text{
+            background-color: #bbbbbb;
+        }
+
+        .padding{
+            height: 100px;
+            background-color: #1c94c4;
+        }
+        .filler{
+            background-color: #536283;
+            height: 1px;
+        }
+        .foller{
+            height: 15px;
+            width: 100%;
+            background-color: #bbb283;
+        }
+            #user_activity label{
+                display: inline-block;
+                border: solid #ffffff 1px;
+                padding: 3px 5px;
+                color: #ffffff;
+            }
+            #user_activity label:hover{
+                cursor: pointer;
+                background-color: #ffffff;
+                color: #cccccc;
+            }
+        #our_info{
+            background-color: #99BC99;
+            text-align: center;
+        }
+            #our_info p{
+                list-style: none;
+                color: #cccccc;
+                font-size: 0.8em;
+                margin: 0 auto;
+            }
+            #our_info ul{
+                list-style: none;
+                display: inline;
+                margin: 0 auto;
+            }
+                #our_info li{
+                    display: inline;
+                    padding: 2px 5px;
+                    margin: 0 auto;
+                }
+        #registration_container{
+            background-color: #999999;
+        }
+            #registration_container fieldset{
+                border: solid #000000 2px;
+                width: 500px;
+                margin: 0 auto;
+                padding: 10px;
+            }
     </style>
 </head>
 
 <body>
-    <div id="ua_window">
-        <!--Aquí va el contenido de lo que suceda con u_a -->
+    <div id="ua_session_window">
+    <!--Aquí va el contenido de lo que suceda con u_a -->
+        <? $this->load->view($user_activity); ?>
+    </div>
+    <div id="ua_registration_window">
+    <!--Aquí va el contenido de lo que suceda con u_a -->
+        <? $this->load->view('modular/ua_registration'); ?>
     </div>
     <div id="wrapper" class="container_12">
         <div id="header" class="grid_12">
-            <div id="logo"          class="grid_5 alpha"></div>
+            <div id="logo"          class="grid_5 alpha">
+
+            </div>
             <div class="grid_5"></div>
             <div id="user_activity" class="grid_2 omega">
-                <? $this->load->view($user_activity); ?>
+                <label id="ua_reg">Regístrate</label>
+                <label id="ua_login">Entra</label>
             </div>
         </div>
         <div class="clear"></div>
         <div id="container" class="grid_12">
             <div class="padding"></div>
-            <div class="grid_1 aplha"></div><!--Container left filler -->
-            <div id="logo" class="grid_10">
+            <div class="filler grid_1 alpha"></div><!--Container left filler -->
+            <div class="grid_10">
                 <div id="heading"       class="grid_4 alpha">
+                    Encabezado
                 </div>
                 <div id="followUp_text" class="grid_6 omega">
+                    <p>Texto de followup</p>
                 </div>
-                <div class="padding"></div>
+                <div class="grid_10 padding"></div>
                 <div id="sandbox" class="grid_10">
-                    <div id="info_container"></div>
-                    <div id="registration_container"></div>
+                    <div id="info_container">
+                        <p>
+                            Información Información Información
+                        </p>
+                    </div>
+                    <div id="registration_container">
+                        <fieldset>
+                            <legend>Regístrate</legend>
+                            <? $this->load->view('modular/ua_registration'); ?>
+                        </fieldset>
+                    </div>
                 </div>
             </div>
-            <div class="grid_1 omega"></div><!--Container rigth filler -->
+            <div class="filler grid_1 omega"></div><!--Container rigth filler -->
         </div>
         <div class="clear"></div>
         <div id="our_info" class="grid_12">
             <ul>
-                <li><?=anchor('info/metodo', 'blah'); ?></li>
-                <li><?=anchor('info/metodo', 'blah'); ?></li>
-                <li><?=anchor('info/metodo', 'blah'); ?></li>
-                <li><?=anchor('info/metodo', 'blah'); ?></li>
+                <li><?=anchor('info/metodo', 'Info 1'); ?></li>
+                <li><?=anchor('info/metodo', 'Info 2'); ?></li>
+                <li><?=anchor('info/metodo', 'Info 3'); ?></li>
+                <li><?=anchor('info/metodo', 'Info 4'); ?></li>
             </ul>
             <p>
                 Una producción de Ramza <br />
